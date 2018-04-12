a2706296f8a354e8a2c22bd6045e6caa67fc0f97
Josef Pihrt <josef.pihrt@outlook.com>
2018-04-13 00:28:19 +0200

    x

--- src/Analyzers/CSharp/Analysis/UnusedMember/UnusedMemberAnalyzer.cs
+++ src/Analyzers/CSharp/Analysis/UnusedMember/UnusedMemberAnalyzer.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
