ff02c5e70e7f8a268230849e31380f4cb1cf8850
Josef Pihrt <josef.pihrt@outlook.com>
2018-04-24 23:26:04 +0200

    x

--- src/Refactorings/CSharp/Refactorings/InlineConstantValueRefactoring.cs
+++ src/Refactorings/CSharp/Refactorings/InlineConstantValueRefactoring.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -20,26 +21,93 @@ namespace Roslynator.CSharp.Refactorings
 
             SemanticModel semanticModel = await context.GetSemanticModelAsync().ConfigureAwait(false);
 
-            Optional<object> optional = semanticModel.GetConstantValue(expression, context.CancellationToken);
+            LiteralExpressionSyntax literalExpression = null;
 
-            if (!optional.HasValue)
+            var optional = default(Optional<object>);
+
+            CancellationToken cancellationToken = context.CancellationToken;
+
+            ISymbol symbol = semanticModel.GetSymbol(expression, cancellationToken);
+
+            if (symbol?.IsErrorType() != false)
                 return;
 
-            SyntaxNode oldNode = expression;
-
-            if (oldNode.IsParentKind(SyntaxKind.SimpleMemberAccessExpression))
+            if (symbol is IFieldSymbol fieldSymbol)
             {
-                var memberAccessExpression = (MemberAccessExpressionSyntax)oldNode.Parent;
+                if (!fieldSymbol.HasConstantValue)
+                    return;
 
-                if (memberAccessExpression.Name == expression)
-                    oldNode = memberAccessExpression;
+                if (fieldSymbol.ContainingType?.TypeKind == TypeKind.Enum)
+                    return;
+
+                optional = new Optional<object>(fieldSymbol.ConstantValue);
+
+                while (fieldSymbol.GetSyntaxOrDefault(cancellationToken) is VariableDeclaratorSyntax variableDeclarator)
+                {
+                    ExpressionSyntax value = variableDeclarator.Initializer.Value;
+
+                    literalExpression = value.WalkDownParentheses() as LiteralExpressionSyntax;
+
+                    if (literalExpression != null)
+                        break;
+
+                    fieldSymbol = semanticModel.GetSymbol(value, cancellationToken) as IFieldSymbol;
+
+                    if (fieldSymbol == null)
+                        break;
+                }
+            }
+            else if (symbol is ILocalSymbol localSymbol)
+            {
+                if (!localSymbol.HasConstantValue)
+                    return;
+
+                optional = new Optional<object>(localSymbol.ConstantValue);
+
+                while (localSymbol.GetSyntaxOrDefault(cancellationToken) is VariableDeclaratorSyntax variableDeclarator)
+                {
+                    ExpressionSyntax value = variableDeclarator.Initializer.Value;
+
+                    literalExpression = value.WalkDownParentheses() as LiteralExpressionSyntax;
+
+                    if (literalExpression != null)
+                        break;
+
+                    localSymbol = semanticModel.GetSymbol(value, cancellationToken) as ILocalSymbol;
+
+                    if (localSymbol == null)
+                        break;
+                }
+            }
+            else
+            {
+                optional = semanticModel.GetConstantValue(expression, cancellationToken);
             }
 
-            ExpressionSyntax newNode = LiteralExpression(optional.Value);
+            if (literalExpression == null)
+            {
+                if (!optional.HasValue)
+                    return;
+
+                literalExpression = LiteralExpression(optional.Value);
+            }
 
             context.RegisterRefactoring(
-                $"Inline constant value",
-                cancellationToken => context.Document.ReplaceNodeAsync(oldNode, newNode.WithTriviaFrom(expression).Parenthesize(), cancellationToken),
+                "Inline constant value",
+                ct =>
+                {
+                    SyntaxNode oldNode = expression;
+
+                    if (oldNode.IsParentKind(SyntaxKind.SimpleMemberAccessExpression))
+                    {
+                        var memberAccessExpression = (MemberAccessExpressionSyntax)oldNode.Parent;
+
+                        if (memberAccessExpression.Name == expression)
+                            oldNode = memberAccessExpression;
+                    }
+
+                    return context.Document.ReplaceNodeAsync(oldNode, literalExpression.WithTriviaFrom(expression).Parenthesize(), ct);
+                },
                 RefactoringIdentifiers.InlineConstantValue);
         }
     }
