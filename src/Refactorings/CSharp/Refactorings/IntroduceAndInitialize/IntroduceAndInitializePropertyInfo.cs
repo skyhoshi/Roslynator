0cadf7232be47f59169e1a377fa0fc8127f372e4
Josef Pihrt <josef.pihrt@outlook.com>
2018-05-14 19:02:44 +0200

    x

--- src/Refactorings/CSharp/Refactorings/IntroduceAndInitialize/IntroduceAndInitializePropertyInfo.cs
+++ src/Refactorings/CSharp/Refactorings/IntroduceAndInitialize/IntroduceAndInitializePropertyInfo.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
-// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using static Roslynator.CSharp.CSharpFactory;
 
