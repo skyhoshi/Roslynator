7139344ccbc58853c9d03093c8304400aac0d60d
Josef Pihrt <josef.pihrt@outlook.com>
2018-05-17 04:05:40 +0200

    x

--- tools/build.cmd
+++ tools/build.cmd
@@ -17,6 +17,12 @@ dotnet "..\src\Tools\CodeGenerator\bin\Release\netcoreapp2.0\CodeGenerator.dll" 
 dotnet "..\src\Tools\VersionUpdater\bin\Release\netcoreapp2.0\VersionUpdater.dll" "1.8.3.0"
 
 "C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild" "..\src\Roslynator.sln" ^
+ /t:Clean ^
+ /p:Configuration=Debug ^
+ /v:minimal ^
+ /m
+
+"C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild" "..\src\Roslynator.sln" ^
  /t:Clean,Build ^
  /p:Configuration=Release,TreatWarningsAsErrors=true,WarningsNotAsErrors=1591 ^
  /v:normal ^
@@ -48,6 +54,8 @@ if errorlevel 1 (
  exit
 )
 
+dotnet pack -c Release --no-build -v normal "..\src\Analyzers\Analyzers.csproj"
+dotnet pack -c Release --no-build -v normal "..\src\CodeFixes\CodeFixes.csproj"
 dotnet pack -c Release --no-build -v normal "..\src\CSharp\CSharp.csproj"
 dotnet pack -c Release --no-build -v normal "..\src\CSharp.Workspaces\CSharp.Workspaces.csproj"
 
