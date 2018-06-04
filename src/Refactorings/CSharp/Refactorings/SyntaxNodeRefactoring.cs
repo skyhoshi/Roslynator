d2a292656f3d93441602410917661f60e690e331
Josef Pihrt <josef.pihrt@outlook.com>
2018-06-05 00:06:35 +0200

    Refactor SyntaxNodeRefactoring.

--- src/Refactorings/CSharp/Refactorings/SyntaxNodeRefactoring.cs
+++ src/Refactorings/CSharp/Refactorings/SyntaxNodeRefactoring.cs
@@ -1,7 +1,11 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
 using Roslynator.CSharp.Refactorings.WrapSelectedLines;
 using Roslynator.Text;
 
@@ -13,18 +17,29 @@ namespace Roslynator.CSharp.Refactorings
         {
             if (context.IsAnyRefactoringEnabled(
                 RefactoringIdentifiers.WrapInRegion,
-                RefactoringIdentifiers.WrapInIfDirective))
+                RefactoringIdentifiers.WrapInIfDirective,
+                RefactoringIdentifiers.RemoveEmptyLines))
             {
-                //XPERF:
-                TextLineCollectionSelection selectedLines = await SelectedLinesRefactoring.GetSelectedLinesAsync(context).ConfigureAwait(false);
+                SyntaxNode root = context.Root;
+                TextSpan span = context.Span;
 
-                if (selectedLines != null)
+                if (!IsFullLineSelection(node, span))
+                    return;
+
+                Document document = context.Document;
+                SourceText sourceText = await document.GetTextAsync(context.CancellationToken).ConfigureAwait(false);
+
+                if (!TextLineCollectionSelection.TryCreate(sourceText.Lines, span, out TextLineCollectionSelection selectedLines))
+                    return;
+
+                if (!IsInMultiLineDocumentationComment(root, span.Start)
+                    && !IsInMultiLineDocumentationComment(root, span.End))
                 {
                     if (context.IsRefactoringEnabled(RefactoringIdentifiers.WrapInRegion))
                     {
                         context.RegisterRefactoring(
                             "Wrap in region",
-                            ct => WrapInRegionRefactoring.Instance.RefactorAsync(context.Document, selectedLines, ct),
+                            ct => WrapInRegionRefactoring.Instance.RefactorAsync(document, selectedLines, ct),
                             RefactoringIdentifiers.WrapInRegion);
                     }
 
@@ -32,20 +47,139 @@ namespace Roslynator.CSharp.Refactorings
                     {
                         context.RegisterRefactoring(
                             "Wrap in #if",
-                            ct => WrapInIfDirectiveRefactoring.Instance.RefactorAsync(context.Document, selectedLines, ct),
+                            ct => WrapInIfDirectiveRefactoring.Instance.RefactorAsync(document, selectedLines, ct),
                             RefactoringIdentifiers.WrapInIfDirective);
+                    }
+                }
+
+                if (context.IsRefactoringEnabled(RefactoringIdentifiers.RemoveEmptyLines))
+                {
+                    bool containsEmptyLine = false;
+
+                    foreach (TextLine line in selectedLines)
+                    {
+                        if (line.IsEmptyOrWhiteSpace()
+                            && root.FindTrivia(line.End, findInsideTrivia: true).IsEndOfLineTrivia())
+                        {
+                            containsEmptyLine = true;
+                            break;
+                        }
+                    }
+
+                    if (containsEmptyLine)
+                    {
+                        context.RegisterRefactoring(
+                            "Remove empty lines",
+                            ct =>
+                            {
+                                IEnumerable<TextChange> textChanges = selectedLines
+                                    .Where(line => line.IsEmptyOrWhiteSpace() && root.FindTrivia(line.End, findInsideTrivia: true).IsEndOfLineTrivia())
+                                    .Select(line => new TextChange(line.SpanIncludingLineBreak, ""));
+
+                                return document.WithTextChangesAsync(textChanges, ct);
+                            },
+                            RefactoringIdentifiers.RemoveEmptyLines);
+                    }
+                }
+            }
+        }
+
+        private static bool IsFullLineSelection(SyntaxNode node, TextSpan span)
+        {
+            if (span.IsEmpty)
+                return false;
+
+            if (!IsStartOfLine(node, span.Start))
+                return false;
+
+            int end = span.End;
+
+            if (node.FullSpan.End == end)
+            {
+                SyntaxTrivia trivia = node.FindTrivia(end - 1);
+
+                return trivia.IsEndOfLineTrivia()
+                    && trivia.Span.End == end;
+            }
+
+            return IsStartOfLine(node, end, orEndOfLine: true);
+        }
+
+        private static bool IsStartOfLine(SyntaxNode node, int position, bool orEndOfLine = false)
+        {
+            SyntaxToken token = node.FindToken(position, findInsideTrivia: true);
+
+            if (orEndOfLine
+                && token.IsKind(SyntaxKind.XmlTextLiteralNewLineToken))
+            {
+                return true;
+            }
+
+            SyntaxTriviaList leadingTrivia = token.LeadingTrivia;
+
+            if (leadingTrivia.Any()
+                && leadingTrivia.Span.Contains(position))
+            {
+                foreach (SyntaxTrivia trivia in leadingTrivia)
+                {
+                    if (trivia.IsEndOfLineTrivia()
+                        && trivia.Span.End == position)
+                    {
+                        return true;
+                    }
+                }
+            }
+            else if (orEndOfLine)
+            {
+                SyntaxTriviaList trailingTrivia = token.TrailingTrivia;
+
+                if (trailingTrivia.Any()
+                    && trailingTrivia.Span.Contains(position))
+                {
+                    foreach (SyntaxTrivia trivia in trailingTrivia)
+                    {
+                        if (trivia.IsEndOfLineTrivia()
+                            && trivia.Span.Start == position)
+                        {
+                            return true;
+                        }
                     }
                 }
             }
 
-            if (context.IsRefactoringEnabled(RefactoringIdentifiers.RemoveEmptyLines)
-                && await RemoveEmptyLinesRefactoring.CanRefactorAsync(context, node).ConfigureAwait(false))
+            SyntaxToken prevToken = token.GetPreviousToken(includeDocumentationComments: true);
+
+            if (prevToken.IsKind(SyntaxKind.XmlTextLiteralNewLineToken)
+                && prevToken.Span.End == position)
             {
-                context.RegisterRefactoring(
-                    "Remove empty lines",
-                    ct => RemoveEmptyLinesRefactoring.RefactorAsync(context.Document, context.Span, ct),
-                    RefactoringIdentifiers.RemoveEmptyLines);
+                return true;
             }
+
+            prevToken = token.GetPreviousToken();
+
+            foreach (SyntaxTrivia trivia in prevToken.TrailingTrivia)
+            {
+                if (trivia.IsEndOfLineTrivia()
+                    && trivia.Span.End == position)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool IsInMultiLineDocumentationComment(SyntaxNode root, int position)
+        {
+            SyntaxToken token = root.FindToken(position, findInsideTrivia: true);
+
+            for (SyntaxNode node = token.Parent; node != null; node = node.Parent)
+            {
+                if (node.IsKind(SyntaxKind.MultiLineDocumentationCommentTrivia))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
