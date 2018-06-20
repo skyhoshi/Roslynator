b6511baca3f786c852e486c6e28d3dc2e120df17
Josef Pihrt <josef.pihrt@outlook.com>
2018-06-20 03:50:40 +0200

    x

--- src/CSharp/CSharp/CSharpFacts.cs
+++ src/CSharp/CSharp/CSharpFacts.cs
@@ -920,5 +920,21 @@ namespace Roslynator.CSharp
                     return 0;
             }
         }
+
+        //TODO: make public
+        internal static bool IsOverridableDeclaration(SyntaxKind kind)
+        {
+            switch (kind)
+            {
+                case SyntaxKind.EventFieldDeclaration:
+                case SyntaxKind.MethodDeclaration:
+                case SyntaxKind.PropertyDeclaration:
+                case SyntaxKind.EventDeclaration:
+                case SyntaxKind.IndexerDeclaration:
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
